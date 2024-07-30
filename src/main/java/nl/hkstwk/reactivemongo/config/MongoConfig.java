b6ead4d4717abf663d533b60233a389b45753cae
hkstwk <hkolvoort@gmail.com>
2024-07-30 18:39:24 +0200

    296 - Add Mongo Database And Client Configuration

---
+++ src/main/java/nl/hkstwk/reactivemongo/config/MongoConfig.java
@@ -0,0 +1,22 @@
+package nl.hkstwk.reactivemongo.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Bean
+    public MongoClient mongoClient(){
+        MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "sfg";
+    }
+
+}
