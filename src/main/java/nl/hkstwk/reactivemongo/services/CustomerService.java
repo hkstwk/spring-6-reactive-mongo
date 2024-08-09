7013bc20ef53958e3ada03b7288e497e7f9c3c3a
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-08-09 17:58:49 +0200

    Assignment 33 - Add Customer Entity And Endpoints

---
+++ src/main/java/nl/hkstwk/reactivemongo/services/CustomerService.java
@@ -0,0 +1,15 @@
+package nl.hkstwk.reactivemongo.services;
+
+import nl.hkstwk.reactivemongo.model.CustomerDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface CustomerService {
+    Flux<CustomerDTO> listCustomers();
+    Mono<CustomerDTO> getCustomerById(String customerId);
+    Mono<CustomerDTO> saveCustomer(Mono<CustomerDTO> customerDTO);
+    Mono<CustomerDTO> saveCustomer(CustomerDTO customerDTO);
+    Mono<CustomerDTO> updateCustomer(String customerId, CustomerDTO customerDTO);
+    Mono<CustomerDTO> patchCustomer(String customerId, CustomerDTO customerDTO);
+    Mono<Void> deleteCustomer(String customerId);
+}
---
+++ src/main/java/nl/hkstwk/reactivemongo/services/CustomerServiceImpl.java
@@ -0,0 +1,56 @@
+package nl.hkstwk.reactivemongo.services;
+
+import lombok.RequiredArgsConstructor;
+import nl.hkstwk.reactivemongo.mappers.CustomerMapper;
+import nl.hkstwk.reactivemongo.model.CustomerDTO;
+import nl.hkstwk.reactivemongo.repositories.CustomerRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerMapper customerMapper;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public Flux<CustomerDTO> listCustomers() {
+       return customerRepository.findAll()
+               .map(customerMapper::customerToCustomerDTO);
+    }
+
+    @Override
+    public Mono<CustomerDTO> getCustomerById(String customerId) {
+        return customerRepository.findById(customerId)
+                .map(customerMapper::customerToCustomerDTO);
+    }
+
+    @Override
+    public Mono<CustomerDTO> saveCustomer(Mono<CustomerDTO> customerDTO) {
+        return customerDTO.map(customerMapper::customerDTOToCustomer)
+                .flatMap(customerRepository::save)
+                .map(customerMapper::customerToCustomerDTO);
+    }
+
+    @Override
+    public Mono<CustomerDTO> saveCustomer(CustomerDTO customerDTO) {
+        return customerRepository.save(customerMapper.customerDTOToCustomer(customerDTO))
+                .map(customerMapper::customerToCustomerDTO);
+    }
+
+    @Override
+    public Mono<CustomerDTO> updateCustomer(String customerId, CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public Mono<CustomerDTO> patchCustomer(String customerId, CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> deleteCustomer(String customerId) {
+        return null;
+    }
+}
