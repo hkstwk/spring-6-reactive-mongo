6c9a60dda4f410d83d9700a4839d36797a11aadb
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-09-09 19:07:27 +0200

    Assignment 39 - Add Logbook to Webflux.fn

--- src/main/java/nl/hkstwk/reactivemongo/config/SecurityConfig.java
+++ src/main/java/nl/hkstwk/reactivemongo/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-
     @Bean
     @Order(1)
     public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception{
