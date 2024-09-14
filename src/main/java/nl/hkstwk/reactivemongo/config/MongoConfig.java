103c9029833a5f6321a6d9172dfa48cb0414eef7
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-09-14 11:56:32 +0200

    445 - Running reactive mongo

--- src/main/java/nl/hkstwk/reactivemongo/config/MongoConfig.java
+++ src/main/java/nl/hkstwk/reactivemongo/config/MongoConfig.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -13,6 +14,9 @@ import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${sfg.mongohost}")
+    String mongoDbHost;
 
     @Bean
     public MongoClient mongoClient(){
@@ -28,7 +32,7 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     protected void configureClientSettings(MongoClientSettings.Builder builder) {
         builder.credential(MongoCredential.createCredential("root", "admin", "example".toCharArray()))
                 .applyToClusterSettings(settings -> {
-                    settings.hosts(Collections.singletonList(new ServerAddress("127.0.0.1", 27017)));
+                    settings.hosts(Collections.singletonList(new ServerAddress(mongoDbHost, 27017)));
                 });
     }
 }
